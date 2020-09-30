241bc8dab66c6840ead49050b9f3a2d88675218e
unknown <ishaqbux24000@gmail.com>
2020-09-30 01:03:19 -0700

    merging

--- serviceworker.js
+++ serviceworker.js
@@ -20,14 +20,7 @@
 // });
 
 const CACHE_NAME = "version-1";
-const urlsToCache = [
-  "index.html",
-  "offline.html",
-  "js",
-  "css",
-  "images",
-  "audio",
-];
+const urlsToCache = ["index.html", "offline.html"];
 const self = this;
 self.addEventListener("install", (event) => {
   event.waitUntil(
